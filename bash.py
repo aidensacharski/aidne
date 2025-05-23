bf08d278a7763458dc0a9bfb9622ca483b74bdd9
aidensacharski <123769728+aidensacharski@users.noreply.github.com>
2025-05-23 14:34:06 -0400

    Update bash.py
    Signed-off-by: aidensacharski <123769728+aidensacharski@users.noreply.github.com>

--- bash.py
+++ bash.py
@@ -2,6 +2,6 @@ git init
 git add .
 git commit -m "Initial commit"
 
-git remote add origin https://github.com/aidensacharski/aidene.git
+git remote add origin https://github.com/aidensacharski/gradeschool.git
 git branch -M main
 git push -u origin main
