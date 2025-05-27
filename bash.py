dc45d8041c5509e2bac1f7af529b48cb75165778
aidensacharski <123769728+aidensacharski@users.noreply.github.com>
2025-05-27 10:25:25 -0400

    Update bash.py
    Signed-off-by: aidensacharski <123769728+aidensacharski@users.noreply.github.com>

--- bash.py
+++ bash.py
@@ -7,3 +7,16 @@ git branch -M main
 git push -u origin main
 
 pip install requests
+
+sudo apt install cloudflared
+cloudflared login
+cloudflared tunnel create my-tunnel
+tunnel: my-tunnel
+credentials-file: /home/youruser/.cloudflared/my-tunnel.json
+
+ingress:
+  - hostname: sacharski.cc
+    service: http://localhost:3000
+  - service: http_status:404
+cloudflared tunnel route dns my-tunnel sacharski.cc
+cloudflared tunnel run my-tunnel
